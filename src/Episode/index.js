e416df8743a046c1530cd48fc1422aca45f578e4
Yura Kushnir <kushnir.yura@gmail.com>
2019-04-08 17:11:57 +0300

    Show episode data

--- src/Episode/index.js
+++ src/Episode/index.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { fetchEpisode } from "../api";
 
 function Episode({ match }) {
-  return <div>Episode {match.params.id}</div>;
+  const [episode, setEpisode] = useState(null);
+
+  useEffect(() => {
+    fetchEpisode(match.params.id).then(data => setEpisode(data));
+  }, []);
+
+  return (
+    episode && (
+      <div>
+        <h2>Episode</h2>
+        Name: {episode.name}
+        Air Date: {episode.air_date}
+      </div>
+    )
+  );
 }
 
 export default Episode;
--- src/EpisodeList/index.js
+++ src/EpisodeList/index.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { fetchEpisodeAll } from "../api";
 
 function EpisodeList() {
-  return <div>EpisodeList</div>;
+  const [episodes, setEpisodes] = useState([]);
+
+  useEffect(() => {
+    fetchEpisodeAll().then(data => setEpisodes(data));
+  }, []);
+
+  return (
+    <>
+      <h2>EpisodeList</h2>
+      <ul>
+        {episodes.map(ep => {
+          return (
+            <li key={ep.id}>
+              <Link to={`/episode/${ep.id}`}>{ep.name}</Link>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
 }
 
 export default EpisodeList;
