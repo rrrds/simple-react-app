5b78320d923ae285f23f0983f967b52d1b75c673
Yura Kushnir <kushnir.yura@gmail.com>
2019-04-08 17:11:46 +0300

    Show character data

--- src/Character/index.js
+++ src/Character/index.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { fetchCharacter } from "../api";
 
 function Character({ match }) {
-  return <div>Character {match.params.id}</div>;
+  const [character, setCharacter] = useState(null);
+
+  useEffect(() => {
+    fetchCharacter(match.params.id).then(data => setCharacter(data));
+  }, []);
+
+  return (
+    character && (
+      <div>
+        <h2>Character</h2>
+        Name: {character.name}
+        Status: {character.status}
+        Species: {character.species}
+        Gender: {character.gender}
+      </div>
+    )
+  );
 }
 
 export default Character;
--- src/CharacterList/index.js
+++ src/CharacterList/index.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { fetchCharacterAll } from "../api";
 
 function CharacterList() {
-  return <div>CharacterList</div>;
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    fetchCharacterAll().then(data => setCharacters(data));
+  }, []);
+
+  return (
+    <>
+      <h2>CharacterList</h2>
+      <ul>
+        {characters.map(char => {
+          return (
+            <li key={char.id}>
+              <Link to={`/character/${char.id}`}>{char.name}</Link>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
 }
 
 export default CharacterList;
