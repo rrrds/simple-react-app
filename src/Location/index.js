8205a29cc5f884de36431b034d18003afc38e140
Yura Kushnir <kushnir.yura@gmail.com>
2019-04-08 17:12:09 +0300

    Show location data

--- src/Location/index.js
+++ src/Location/index.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { fetchLocation } from "../api";
 
 function Location({ match }) {
-  return <div>Location {match.params.id}</div>;
+  const [location, setLocation] = useState(null);
+
+  useEffect(() => {
+    fetchLocation(match.params.id).then(data => setLocation(data));
+  }, []);
+
+  return (
+    location && (
+      <div>
+        <h2>Location</h2>
+        Name: {location.name}
+      </div>
+    )
+  );
 }
 
 export default Location;
--- src/LocationList/index.js
+++ src/LocationList/index.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { fetchLocationAll } from "../api";
 
 function LocationList() {
-  return <div>LocationList</div>;
+  const [locations, setLocations] = useState([]);
+
+  useEffect(() => {
+    fetchLocationAll().then(data => setLocations(data));
+  }, []);
+
+  return (
+    <>
+      <h2>LocationList</h2>
+      <ul>
+        {locations.map(loc => {
+          return (
+            <li key={loc.id}>
+              <Link to={`/location/${loc.id}`}>{loc.name}</Link>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
 }
 
 export default LocationList;
