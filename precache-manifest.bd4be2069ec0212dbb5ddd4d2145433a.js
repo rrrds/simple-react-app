4df5a400deca88ddcd4fbc27225b7ba07d8b6e1d
Yura Kushnir <kushnir.yura@gmail.com>
2019-04-12 12:10:37 +0300

    Updates

---
+++ precache-manifest.bd4be2069ec0212dbb5ddd4d2145433a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3278aec368d961615c34995c612380f1",
+    "url": "/simple-react-app/static/media/hero.3278aec3.jpg"
+  },
+  {
+    "revision": "b3214938f708e55ad090",
+    "url": "/simple-react-app/static/js/runtime~main.d088d395.js"
+  },
+  {
+    "revision": "3c935a02fd780f6ac88d",
+    "url": "/simple-react-app/static/js/main.172a6f0c.chunk.js"
+  },
+  {
+    "revision": "f36e7a6bc845c12df35e",
+    "url": "/simple-react-app/static/js/2.76abba8d.chunk.js"
+  },
+  {
+    "revision": "3c935a02fd780f6ac88d",
+    "url": "/simple-react-app/static/css/main.cbfba78d.chunk.css"
+  },
+  {
+    "revision": "411535d660fb1b588a433c3227acbbe7",
+    "url": "/simple-react-app/index.html"
+  }
+];
